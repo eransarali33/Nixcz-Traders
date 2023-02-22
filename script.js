f028fa9a20e7a0afc57f66061bbdc1dd6f2acc12
Ansar Ali <eransarali33@gmail.com>
2023-02-22 23:16:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// ------------------------Scroll Button------------------------
+
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
